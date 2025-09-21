dd9528d7a6d958775fa1a43980fee81a294d0464
KTibow <KTibow@users.noreply.github.com>
2025-09-20 20:53:21 -0700

    google test

--- src/resources/browser-test.remote.ts
+++ src/resources/browser-test.remote.ts
@@ -1,12 +1,19 @@
-import { launch } from "@astral/astral";
+import { type ElementHandle, launch } from "@astral/astral";
 import { fn } from "monoserve";
 
 export default fn(async () => {
   // Launch the browser
-  await using browser = await launch({ args: ["--no-sandbox"] });
+  await using browser = await launch({
+    args: ["--no-sandbox"],
+    launchPresets: { windowSize: { width: 1280, height: 720 } },
+  });
 
   // Open a new page
-  await using page = await browser.newPage("https://deno.land");
+  await using page = await browser.newPage("https://nsd.schoology.com");
+
+  const emailInput = (await page.$('input[type="email"]')) as ElementHandle;
+  await emailInput.type("1000000@apps.nsd.org", { delay: 100 });
+  await emailInput.type("\n");
 
   const screenshot = await page.screenshot();
 
