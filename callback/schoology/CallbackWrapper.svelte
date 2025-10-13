81fb96b7169bbf7356c2f4d86586be457cefc88b
KTibow <KTibow@users.noreply.github.com>
2025-10-12 18:29:47 -0700

    new monoidentity

--- callback/schoology/CallbackWrapper.svelte
+++ callback/schoology/CallbackWrapper.svelte
@@ -3,6 +3,10 @@
   import Callback from "./Callback.svelte";
 </script>
 
-<Monoidentity app="secant" intents={[{ loginRecognized: true }]}>
+<Monoidentity
+  app="secant"
+  intents={[{ loginRecognized: true }]}
+  shouldBackup={(path) => path.startsWith(".core") || path.startsWith(".config/secant")}
+>
   <Callback />
 </Monoidentity>
--- src/AppWrapper.svelte
+++ src/AppWrapper.svelte
@@ -4,7 +4,11 @@
   import App from "./App.svelte";
 </script>
 
-<Monoidentity app="secant" intents={[{ loginRecognized: true }]}>
+<Monoidentity
+  app="secant"
+  intents={[{ loginRecognized: true }]}
+  shouldBackup={(path) => path.startsWith(".core") || path.startsWith(".config/secant")}
+>
   <svelte:boundary>
     {#snippet failed(error)}
       <ErrorAlert {error} />
