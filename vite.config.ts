85ebc2b1ce9315d42c695f50a668c44af2343ccc
KTibow <KTibow@users.noreply.github.com>
2026-02-21 17:04:08 -0800

    switch to cf workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,36 @@
 import { defineConfig } from "vite";
+import type { Plugin } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { functionsMixins } from "vite-plugin-functions-mixins";
 import { monoserve } from "monoserve/plugin";
+import { build as rolldownBuild } from "rolldown";
+import { importGlobPlugin } from "rolldown/experimental";
+
+const buildCloudflareWorker = (): Plugin => ({
+  name: "build-cloudflare-worker",
+  apply: "build",
+  closeBundle: {
+    sequential: true,
+    async handler() {
+      await rolldownBuild({
+        input: "worker.ts",
+        plugins: [importGlobPlugin()],
+        output: {
+          file: "dist-worker.js",
+          format: "esm",
+          inlineDynamicImports: true,
+        },
+      });
+    },
+  },
+});
 
 export default defineConfig({
   plugins: [
     svelte(),
     functionsMixins({ deps: ["m3-svelte"] }),
-    monoserve({ monoserverURL: "https://benignmonoserver.fly.dev" }),
+    monoserve({ monoserverURL: "/__monoserve/" }),
+    buildCloudflareWorker(),
   ],
   build: {
     rollupOptions: {
